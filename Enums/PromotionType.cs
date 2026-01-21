f0cf47d8e287a5fa4589121c2424266845a9f929
Thiahho <thiagobrizuelav@gmail.com>
2026-01-20 23:13:10 -0300

    200125

--- Enums/PromotionType.cs
+++
@@ -1,6 +0,0 @@
-namespace EcommerceFS2026.Enums;
-
-public enum PromotionType
-{
-    Percentage = 1
-}
--- Enums/StockReservationStatus.cs
+++
@@ -1,8 +0,0 @@
-namespace EcommerceFS2026.Enums;
-
-public enum StockReservationStatus
-{
-    Active = 1,
-    Consumed = 2,
-    Expired = 3
-}
