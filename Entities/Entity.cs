bfefc95d7744f5da01e2baeef0cbe6ccc8d158ae
Thiahho <thiagobrizuelav@gmail.com>
2026-01-20 23:15:11 -0300

    200125

--- Entities/Entity.cs
+++
@@ -1,8 +0,0 @@
-namespace EcommerceFS2026.Entities;
-
-public abstract class Entity
-{
-    public Guid Id { get; set; } = Guid.NewGuid();
-    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
-    public DateTimeOffset? UpdatedAt { get; set; }
-}
